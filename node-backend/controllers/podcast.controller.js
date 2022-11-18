ccfac62c9032a3337b102ca2db4948eaf17139a5
unknown <adetunmbikenny@gmail.com>
2022-11-18 11:08:29 +0100

    created the podcast uploader route

---
+++ node-backend/controllers/podcast.controller.js
@@ -0,0 +1,29 @@
+const { writeFile } = require('fs/promises');
+const fs = require('fs');
+const Podcast = require('./../models/Podcast');
+
+exports.podcastuploader = async (req, res) => {
+  let podcast = await Podcast.create({
+    user_id: req.headers.user_id,
+    file_path: '///',
+  });
+
+  const user_file_path = './uploads/podcasts/' + req.headers.user_id + '/';
+
+  const podcast_file_path =
+    user_file_path + '-' + podcast.id + '-' + req.file.originalname;
+
+  if (!fs.existsSync(user_file_path)) {
+    fs.mkdirSync(user_file_path);
+  }
+
+  podcast = await Podcast.findOneAndUpdate(
+    { id: podcast._id, user_id: req.headers.user_id },
+    {
+      file_path: podcast_file_path,
+    }
+  );
+
+  await writeFile(podcast_file_path, req.file.buffer);
+  res.send(podcast);
+};
