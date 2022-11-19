9227c57591234755ce25fb523b7e6694c60bce2f
unknown <adetunmbikenny@gmail.com>
2022-11-19 10:14:56 +0100

    patching package.json

--- node-backend/docs/podcasts/index.js
+++ node-backend/docs/podcasts/index.js
@@ -1,24 +1,6 @@
 module.exports = {
   paths: {
-    '/podcasts/upload': {
-      get: {
-        tags: ['Podcasts'],
-        description: 'Get todos',
-        operationId: 'getTodos',
-        parameters: [],
-        responses: {
-          200: {
-            description: 'Todos were obtained',
-            content: {
-              'application/json': {
-                schema: {
-                  $ref: '#/components/schemas/Todo',
-                },
-              },
-            },
-          },
-        },
-      },
+    '/podcasts': {
       post: {
         tags: ['Podcast'],
         description: 'Upload a podcast',
@@ -36,104 +18,6 @@ module.exports = {
         responses: {
           201: {
             description: 'Podcast created successfully',
-          },
-          500: {
-            description: 'Server error',
-          },
-        },
-      },
-    },
-    '/todos/{id}': {
-      get: {
-        tags: ['Foods CRUD operations'],
-        description: 'Get a todo',
-        operationId: 'getTodo',
-        parameters: [
-          // {
-          //   name: 'id',
-          //   in: 'path',
-          //   schema: {
-          //     $ref: '#/components/schemas/id',
-          //   },
-          //   required: true,
-          //   description: 'A single todo id',
-          // },
-        ],
-        responses: {
-          200: {
-            description: 'Todo is obtained',
-            content: {
-              'application/json': {
-                schema: {
-                  $ref: '#/components/schemas/Todo',
-                },
-              },
-            },
-          },
-          404: {
-            description: 'Todo is not found',
-            content: {
-              'application/json': {
-                schema: {
-                  $ref: '#/components/schemas/Error',
-                  example: {
-                    message: "We can't find the todo",
-                    internal_code: 'Invalid id',
-                  },
-                },
-              },
-            },
-          },
-        },
-      },
-      put: {
-        tags: ['Todo CRUD operations'],
-        description: 'Update todo',
-        operationId: 'updateTodo',
-        parameters: [
-          {
-            name: 'id',
-            in: 'path',
-            schema: {
-              $ref: '#/components/schemas/id',
-            },
-            required: true,
-            description: 'Id of todo to be updated',
-          },
-        ],
-        responses: {
-          200: {
-            description: 'Todo updated successfully',
-          },
-          404: {
-            description: 'Todo not found',
-          },
-          500: {
-            description: 'Server error',
-          },
-        },
-      },
-      delete: {
-        tags: ['Todo CRUD operations'],
-        description: 'Deleting a todo',
-        operationId: 'deleteTodo',
-        parameters: [
-          {
-            name: 'id',
-            in: 'path',
-            schema: {
-              $ref: '#/components/schemas/id',
-            },
-            required: true,
-            description: 'Deleting a done todo',
-          },
-        ],
-        responses: {
-          200: {
-            description: 'Todo deleted successfully',
-          },
-          404: {
-            description: 'Todo not found',
           },
           500: {
             description: 'Server error',
