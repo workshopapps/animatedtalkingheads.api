3e01a9c887ac30163a3e7098beafdb13104943a4
Dickson Joseph <ochekayjay@gmail.com>
2022-11-19 19:51:50 +0100

    edited swagger

--- node-backend/docs/podcasts/index.js
+++ node-backend/docs/podcasts/index.js
@@ -25,5 +25,55 @@ module.exports = {
         },
       },
     },
+    '/podcasts/{userid}': {
+      get: {
+        tags: ['Podcast'],
+        description: 'get all podcasts',
+        operationId: 'getPodcast',
+        parameters: [
+          {
+            in: 'formData',
+            name: 'avatar',
+            type: 'file',
+            description: 'to get podcast uploaded.',
+          },
+        ],
+        consumes: ['multipart/form-data'],
+
+        responses: {
+          201: {
+            description: 'Podcast delivered',
+          },
+          500: {
+            description: 'Server error',
+          },
+        },
+      },
+    },
+    '/podcasts/{podcastid}': {
+      delete: {
+        tags: ['Podcast'],
+        description: 'delete a podcast',
+        operationId: 'deletePodcast',
+        parameters: [
+          {
+            in: 'formData',
+            name: 'avatar',
+            type: 'file',
+            description: 'The file to upload.',
+          },
+        ],
+        consumes: ['multipart/form-data'],
+
+        responses: {
+          201: {
+            description: 'Podcast deleted successfully',
+          },
+          500: {
+            description: 'Server error',
+          },
+        },
+      },
+    },
   },
 };
