7c1b0285c12a8f8ac0f2d54514a84fb388a5ba59
Dickson Joseph <ochekayjay@gmail.com>
2022-11-20 16:23:17 +0100

    made more swagger changes

--- node-backend/docs/avatars/index.js
+++ node-backend/docs/avatars/index.js
@@ -166,5 +166,99 @@ module.exports = {
         },
       },
     },
+    '/podcast/{userid}': {
+      get: {
+        tags: ['fetch all podcasts of user'],
+        description: "Get user's podcasts",
+        operationId: 'user id',
+        parameters: [
+          {
+            name: 'id',
+            in: 'path',
+            schema: {
+              $ref: '#/components/schemas/id',
+            },
+            required: true,
+            description: 'user id',
+          },
+        ],
+        responses: {
+          200: {
+            description: 'user podcasts fetched',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/Todo',
+                  example: {
+                    _id: "8gfg8iu89ikjgi5",
+                    user_id: 'kdf84ur8uf485u8u',
+                    file_path: 'filepath/podcast_id/filepathname'
+                  },
+                },
+              },
+            },
+          },
+          404: {
+            description: 'Todo is not found',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/Error',
+                  example: {
+                    message: "We can't find the todo",
+                    internal_code: 'Invalid id',
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    '/podcast/{podcastid}': {
+      delete: {
+        tags: ['delete a podcast of user'],
+        description: "delete user's podcast",
+        operationId: 'podcast id',
+        parameters: [
+          {
+            name: 'id',
+            in: 'path',
+            schema: {
+              $ref: '#/components/schemas/id',
+            },
+            required: true,
+            description: 'podcast id',
+          },
+        ],
+        responses: {
+          200: {
+            description: 'user podcasts deleted',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/Todo',
+                 
+                },
+              },
+            },
+          },
+          404: {
+            description: 'Todo is not found',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/Error',
+                  example: {
+                    message: "We can't find the todo",
+                    internal_code: 'Invalid id',
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
   },
 };
