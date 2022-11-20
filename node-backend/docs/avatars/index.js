ee1e688b8753467bfb7e4d2d7c6fc6e6cf8da80f
Adetunmbi Kehinde <52609237+kenneropia@users.noreply.github.com>
2022-11-20 08:26:25 +0100

    Update index.js

--- node-backend/docs/avatars/index.js
+++ node-backend/docs/avatars/index.js
@@ -1,145 +1,170 @@
 module.exports = {
-  // paths: {
-  //   '/todos': {
-  //     get: {
-  //       tags: ['Avatars'],
-  //       description: 'Get todos',
-  //       operationId: 'getTodos',
-  //       parameters: [],
-  //       responses: {
-  //         200: {
-  //           description: 'Todos were obtained',
-  //           content: {
-  //             'application/json': {
-  //               schema: {
-  //                 $ref: '#/components/schemas/Todo',
-  //               },
-  //             },
-  //           },
-  //         },
-  //       },
-  //     },
-  //     post: {
-  //       tags: ['Todo CRUD operations'],
-  //       description: 'Create todo',
-  //       operationId: 'createTodo',
-  //       parameters: [],
-  //       requestBody: {
-  //         content: {
-  //           'application/json': {
-  //             schema: {
-  //               $ref: '#/components/schemas/TodoInput',
-  //             },
-  //           },
-  //         },
-  //       },
-  //       responses: {
-  //         201: {
-  //           description: 'Todo created successfully',
-  //         },
-  //         500: {
-  //           description: 'Server error',
-  //         },
-  //       },
-  //     },
-  //   },
-  //   '/todos/{id}': {
-  //     get: {
-  //       tags: ['Foods CRUD operations'],
-  //       description: 'Get a todo',
-  //       operationId: 'getTodo',
-  //       parameters: [
-  //         {
-  //           name: 'id',
-  //           in: 'path',
-  //           schema: {
-  //             $ref: '#/components/schemas/id',
-  //           },
-  //           required: true,
-  //           description: 'A single todo id',
-  //         },
-  //       ],
-  //       responses: {
-  //         200: {
-  //           description: 'Todo is obtained',
-  //           content: {
-  //             'application/json': {
-  //               schema: {
-  //                 $ref: '#/components/schemas/Todo',
-  //               },
-  //             },
-  //           },
-  //         },
-  //         404: {
-  //           description: 'Todo is not found',
-  //           content: {
-  //             'application/json': {
-  //               schema: {
-  //                 $ref: '#/components/schemas/Error',
-  //                 example: {
-  //                   message: "We can't find the todo",
-  //                   internal_code: 'Invalid id',
-  //                 },
-  //               },
-  //             },
-  //           },
-  //         },
-  //       },
-  //     },
-  //     put: {
-  //       tags: ['Todo CRUD operations'],
-  //       description: 'Update todo',
-  //       operationId: 'updateTodo',
-  //       parameters: [
-  //         {
-  //           name: 'id',
-  //           in: 'path',
-  //           schema: {
-  //             $ref: '#/components/schemas/id',
-  //           },
-  //           required: true,
-  //           description: 'Id of todo to be updated',
-  //         },
-  //       ],
-  //       responses: {
-  //         200: {
-  //           description: 'Todo updated successfully',
-  //         },
-  //         404: {
-  //           description: 'Todo not found',
-  //         },
-  //         500: {
-  //           description: 'Server error',
-  //         },
-  //       },
-  //     },
-  //     delete: {
-  //       tags: ['Todo CRUD operations'],
-  //       description: 'Deleting a todo',
-  //       operationId: 'deleteTodo',
-  //       parameters: [
-  //         {
-  //           name: 'id',
-  //           in: 'path',
-  //           schema: {
-  //             $ref: '#/components/schemas/id',
-  //           },
-  //           required: true,
-  //           description: 'Deleting a done todo',
-  //         },
-  //       ],
-  //       responses: {
-  //         200: {
-  //           description: 'Todo deleted successfully',
-  //         },
-  //         404: {
-  //           description: 'Todo not found',
-  //         },
-  //         500: {
-  //           description: 'Server error',
-  //         },
-  //       },
-  //     },
-  //   },
-  // },
+  paths: {    '/podcasts/upload': {
+     
+      post: {
+        tags: ['Podcast'],
+        description: 'Upload a podcast',
+        operationId: 'uploadPodcast',
+        parameters: [
+          {
+            in: 'formData',
+            name: 'avatar',
+            type: 'file',
+            description: 'The file to upload.',
+          },
+        ],
+        consumes: ['multipart/form-data'],
+
+        responses: {
+          201: {
+            description: 'Podcast created successfully',
+          },
+          500: {
+            description: 'Server error',
+          },
+        },
+      },
+    },
+    '/avatars': {
+      get: {
+        tags: ['Avatars'],
+        description: 'Get avatars',
+        operationId: 'getAvatar',
+        parameters: [],
+        responses: {
+          200: {
+            description: 'avatars were obtained',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/Todo',
+                },
+              },
+            },
+          },
+        },
+      },
+      post: {
+        tags: ['Avatar post'],
+        description: 'Create Avatar',
+        operationId: 'createAvatar',
+        parameters: [],
+        requestBody: {
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/components/schemas/AvatarInput',
+              },
+            },
+          },
+        },
+        responses: {
+          201: {
+            description: 'Avatar created successfully',
+          },
+          500: {
+            description: 'Server error',
+          },
+        },
+      },
+    },
+    '/avatars/{id}': {
+      get: {
+        tags: ['fetch specific avatar'],
+        description: "Get user's avatar",
+        operationId: 'user avatar id',
+        parameters: [
+          {
+            name: 'id',
+            in: 'path',
+            schema: {
+              $ref: '#/components/schemas/id',
+            },
+            required: true,
+            description: 'avatar id',
+          },
+        ],
+        responses: {
+          200: {
+            description: 'user avatar fetched',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/Todo',
+                },
+              },
+            },
+          },
+          404: {
+            description: 'Todo is not found',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: '#/components/schemas/Error',
+                  example: {
+                    message: "We can't find the todo",
+                    internal_code: 'Invalid id',
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      put: {
+        tags: ['Todo CRUD operations'],
+        description: 'Update todo',
+        operationId: 'updateTodo',
+        parameters: [
+          {
+            name: 'id',
+            in: 'path',
+            schema: {
+              $ref: '#/components/schemas/id',
+            },
+            required: true,
+            description: 'Id of todo to be updated',
+          },
+        ],
+        responses: {
+          200: {
+            description: 'Todo updated successfully',
+          },
+          404: {
+            description: 'Todo not found',
+          },
+          500: {
+            description: 'Server error',
+          },
+        },
+      },
+      delete: {
+        tags: ['Todo CRUD operations'],
+        description: 'Deleting a todo',
+        operationId: 'deleteTodo',
+        parameters: [
+          {
+            name: 'id',
+            in: 'path',
+            schema: {
+              $ref: '#/components/schemas/id',
+            },
+            required: true,
+            description: 'Deleting a done todo',
+          },
+        ],
+        responses: {
+          200: {
+            description: 'Todo deleted successfully',
+          },
+          404: {
+            description: 'Todo not found',
+          },
+          500: {
+            description: 'Server error',
+          },
+        },
+      },
+    },
+  },
 };
