8fcbe25c7413b15e7974c4f86ed4c84414beeb71
Justin <justakinyi@gmail.com>
2022-11-20 17:43:48 +0300

    feat(BAC-24):reversed image generation architecture

--- pyhton-backend/src/podcast_animator/generator/components/parser_two.py
+++ pyhton-backend/src/podcast_animator/generator/components/parser_two.py
@@ -36,5 +36,37 @@ def generate_sequence(url: str):
     sequence = speakers_sequence(audio_data, audiolength)
     return sequence
     
+def speakers_sequence(
+    audio_data: list[Speech], audiolength: int
+) -> dict[str, list]:
+    """taking data from audio transcript and turns into dictionary
+    puts all utterances into a list for easy adapataion
+    @author: justin
+    Args:
+        list: diarized data
+        audiolegth: length of audio
 
+    Returns:
+        List: dict of speaking moments
+    """
+    speaker_sequence = {}
+    for data in audio_data:
+        if data.speaker not in speaker_sequence:
+            speaker_sequence[data.speaker]=[data.duration]
+        else:
+            speaker_sequence[data.speaker].append(data.duration)
+    
 
+    speaking_moments={}
+    for each_speaker in speaker_sequence:
+        # phrase_map = [speaker_tuple[1] for speaker_tuple in each_speaker]
+        flattened_list = list(chain.from_iterable(speaker_sequence[each_speaker]))
+        result = []
+        for i in range(0, audiolength + 1):
+            if i in flattened_list:
+                result.append("speech")
+            else:
+                result.append("silence")
+        speaking_moments[each_speaker] = result
+    return speaking_moments
+    
