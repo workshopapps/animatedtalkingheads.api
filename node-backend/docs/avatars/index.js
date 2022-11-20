2b1a25f51c1e54492f97fae3e6fd8b4b26fce1c8
Adetunmbi Kehinde <52609237+kenneropia@users.noreply.github.com>
2022-11-20 08:25:04 +0100

    Update index.js

--- node-backend/docs/avatars/index.js
+++ node-backend/docs/avatars/index.js
@@ -1,5 +1,30 @@
 module.exports = {
-  paths: {
+  paths: {    '/podcasts/upload': {
+     
+      post: {
+        tags: ['Podcast'],
+        description: 'Upload a podcast',
+        operationId: 'uploadPodcast',
+        parameters: [
+          {
+            in: 'formData',
+            name: 'avatar',
+            type: 'file',
+            description: 'The file to upload.',
+          },
+        ],
+        consumes: ['multipart/form-data'],
+
+        responses: {
+          201: {
+            description: 'Podcast created successfully',
+          },
+          500: {
+            description: 'Server error',
+          },
+        },
+      },
+    },
     '/avatars': {
       get: {
         tags: ['Avatars'],
