e039d6298a86d9f7fcc4a5af1b213fce43bdb676
deelaurence <laurie143pelumi@gmail.com>
2022-12-01 22:22:57 +0100

    added payment gateway with paystack

--- node-backend/index.js
+++ node-backend/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path')
+const pug = require('pug')
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
@@ -54,6 +56,19 @@ app.use(cors());
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(docs));
 app.use('/avatars', avatarRouter);
 app.use('/podcasts', podcastRouter);
+
+///// payment route
+const paymentRoute = require('./routes/payment/index')
+app.use(express.static(path.join(__dirname, 'public/')));
+app.set('view engine', pug);
+app.get('/',(req, res) => {
+    res.render('index.pug');
+});
+app.get('/error', (req, res)=>{
+    res.render('error.pug');
+})
+app.use('/',paymentRoute)
+
 
 app.all('*', (req, res, next) => {
   next(new NotFound());
