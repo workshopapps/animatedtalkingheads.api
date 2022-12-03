4371171e1cd88d58c737a5049b3406eeba3ec792
unknown <adetunmbikenny@gmail.com>
2022-12-03 18:17:30 +0100

    fixed file upload bug

--- node-backend/controllers/run-python/processing.js
+++ node-backend/controllers/run-python/processing.js
@@ -1,28 +1,28 @@
 const { PythonShell } = require('python-shell');
 const path = require('path');
 
-export const runPy = async ({ jobConfig }) => {
+module.exports = async ({ data: { jobConfig } }) => {
+  console.log(jobConfig);
   let options = {
     mode: 'text',
+    // pythonPath: 'c/Users/Hi/AppData/Local/Microsoft/WindowsApps/python3',
     pythonOptions: ['-u'],
+    scriptPath: path.resolve(
+      path.dirname(process.cwd() + '/') +
+        '/pyhton-backend/src/podcast_animator/generator/'
+    ),
 
     args: [jobConfig.meta_json_file],
   };
 
   return new Promise(function (resolve, reject) {
-    PythonShell.run(
-      path.resolve(
-        path.dirname(process.cwd() + '/') +
-          'pyhton-backend/src/podcast_animator/generator/main.py'
-      ),
-      options,
-      function (err, res) {
-        if (err) {
-          console.error(err);
-          reject({ err });
-        }
-        resolve({ success: true, jobConfig });
+    PythonShell.run('/main.py', options, function (err, res) {
+      if (err) {
+        console.error(err);
+        return reject({ err });
       }
-    );
+      console.log('success');
+      resolve({ success: true, jobConfig });
+    });
   });
 };
