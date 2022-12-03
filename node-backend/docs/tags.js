7b42636f0ef29c31d9bb560932223dd47b8c989c
Justin Akinyi <justakinyi1@gmail.com>
2022-12-03 22:06:23 +0300

    Update tags.js

--- node-backend/docs/tags.js
+++ node-backend/docs/tags.js
@@ -3,5 +3,6 @@ module.exports = {
     { name: 'Podcasts', description: 'Access to Podcast' },
     { name: 'Avatars', description: 'Access to Avatars' },
     { name: 'Payment', description: 'Using the paystack payment option' },
+    {name: 'Authentication', description: 'Access to Authentication'},
   ],
 };
