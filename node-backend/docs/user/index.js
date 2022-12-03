1320c3107f1a6ddfee0bbe2c64ca0ffda85b3fe7
Justin Akinyi <justakinyi1@gmail.com>
2022-12-03 22:28:25 +0300

    Update index.js

--- node-backend/docs/user/index.js
+++ node-backend/docs/user/index.js
@@ -5,11 +5,6 @@ module.exports = {
           tags: ['Authentication'],
           description: 'create user account',
           operationId: 'createUserAccount',
-          parameters: [{
-            "email": "default@gmail.com",
-            "password": "gladyoucame" 
-          }
-          ],
           requestBody: {
               content: {
                 'application/json': {
@@ -35,10 +30,6 @@ module.exports = {
           tags: ['Authentication'],
           description: 'Clear user password',
           operationId: 'clearUserPassword',
-          parameters: [{
-            "email": "default@gmail.com",
-            "password": "gladyoucame" 
-          }],
           requestBody: {
               content: {
                 'application/json': {
@@ -64,10 +55,6 @@ module.exports = {
           tags: ['Authentication'],
           description: 'Log out user',
           operationId: 'logOutUser',
-          parameters: [{
-            "email": "default@gmail.com",
-            "password": "gladyoucame" 
-          }],
           requestBody: {
               content: {
                 'application/json': {
@@ -93,129 +80,6 @@ module.exports = {
           tags: ['Authentication'],
           description: 'Log in user',
           operationId: 'logInUser',
-          parameters: [{
-            "email": "default@gmail.com",
-            "password": "gladyoucame" 
-          }],
-          requestBody: {
-              content: {
-                'application/json': {
-                  schema: {
-                    $ref: '#/components/schemas/UsersAuth',
-                  },
-                },
-              },
-            },
-          responses: {
-            200{name: 'Authentication', description: 'Access to Authentication'},: {
-              description: 'User logged in Successfully',
-            },
-            500: {
-              description: 'Server error',
-            },
-          },
-        },
-      },
-    }
-  }module.exports = {
-    paths: {
-      '/auth/signin': {
-        post: {
-          tags: ['Authentication'],
-          description: 'create user account',
-          operationId: 'createUserAccount',
-          parameters: [{
-            "email": "default@gmail.com",
-            "password": "gladyoucame" 
-          }
-          ],
-          requestBody: {
-              content: {
-                'application/json': {
-                  schema: {
-                    $ref: '#/components/schemas/UsersAuth',
-                  },
-                },
-              },
-            },
-          responses: {
-            201: {
-              description: 'User account Created Successfully',
-            },
-            500: {
-              description: 'Server error',
-            },
-          },
-        },
-      },
-
-      '/auth/forgetpassword': {
-        post: {
-          tags: ['Authentication'],
-          description: 'Clear user password',
-          operationId: 'clearUserPassword',
-          parameters: [{
-            "email": "default@gmail.com",
-            "password": "gladyoucame" 
-          }],
-          requestBody: {
-              content: {
-                'application/json': {
-                  schema: {
-                    $ref: '#/components/schemas/UsersAuth',
-                  },
-                },
-              },
-            },
-          responses: {
-            201: {
-              description: 'password cleared successfully',
-            },
-            500: {
-              description: 'Server error',
-            },
-          },
-        },
-      },
-
-      '/auth/logout': {
-        get: {
-          tags: ['Authentication'],
-          description: 'Log out user',
-          operationId: 'logOutUser',
-          parameters: [{
-            "email": "default@gmail.com",
-            "password": "gladyoucame" 
-          }],
-          requestBody: {
-              content: {
-                'application/json': {
-                  schema: {
-                    $ref: '#/components/schemas/UsersAuth',
-                  },
-                },
-              },
-            },
-          responses: {
-            201: {
-              description: 'Logged out Successfully',
-            },
-            500: {
-              description: 'Server error',
-            },
-          },
-        },
-      },
-
-      '/auth/login': {
-        post: {
-          tags: ['Authentication'],
-          description: 'Log in user',
-          operationId: 'logInUser',
-          parameters: [{
-            "email": "default@gmail.com",
-            "password": "gladyoucame" 
-          }],
           requestBody: {
               content: {
                 'application/json': {
