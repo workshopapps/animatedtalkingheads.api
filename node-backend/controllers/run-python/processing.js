62c5ad0f3fcac549f5e9a36d545e81c35caf082f
unknown <adetunmbikenny@gmail.com>
2022-12-04 17:21:01 +0100

    fixed merging issues

--- node-backend/controllers/run-python/processing.js
+++ node-backend/controllers/run-python/processing.js
@@ -2,11 +2,11 @@ const { PythonShell } = require('python-shell');
 const path = require('path');
 
 module.exports = async ({ data: { jobConfig } }) => {
-  console.log(jobConfig);
   let options = {
     mode: 'text',
     pythonPath: path.resolve(
-      'c:\\Users\\Hi\\AppData\\Local\\Programs\\Python\\Python310\\python'
+      path.dirname(process.cwd() + '/') +
+        '/pyhton-backend/venv/Scripts/python.exe'
     ),
     pythonOptions: ['-u'],
     scriptPath: path.resolve(
