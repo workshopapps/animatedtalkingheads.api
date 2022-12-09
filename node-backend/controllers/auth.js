c1f0fa0fc6a351d7509a32542f6896e0299840aa
Just-Akinyi <justakinyi1@gmail.com>
2022-12-09 12:43:40 +0300

    fixes

--- node-backend/controllers/auth.js
+++ node-backend/controllers/auth.js
@@ -89,20 +89,20 @@ module.exports.logout_get = (req, res) => {
   res.status(200).json({ message: 'successfully logged out' });
 };
 
-module.exports.forgetpassword_post = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await UserAuth.update(
-      { email, password },
-      {
-        $set: { password: password },
-      }
-    );
-    const token = createToken(UserAuth._id);
-    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
-    res.status(201).json({ user: UserAuth._id });
-  } catch (err) {
-    const errors = handleErrors(err);
-    res.status(400).json({ errors });
-  }
-};
+// module.exports.forgetpassword_post = async (req, res) => {
+//   const { email, password } = req.body;
+//   try {
+//     const user = await UserAuth.update(
+//       { email, password },
+//       {
+//         $set: { password: password },
+//       }
+//     );
+//     const token = createToken(UserAuth._id);
+//     res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+//     res.status(201).json({ user: UserAuth._id });
+//   } catch (err) {
+//     const errors = handleErrors(err);
+//     res.status(400).json({ errors });
+//   }
+// };
