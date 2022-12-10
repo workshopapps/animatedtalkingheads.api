7adba991bc9d45a1d77bc7e4b83dced2bb6e871c
unknown <adetunmbikenny@gmail.com>
2022-12-10 05:57:19 +0100

    bug fixes

--- node-backend/controllers/avatars.js
+++ node-backend/controllers/avatars.js
@@ -1,59 +1,56 @@
-const Avatars = require('../models/Avatar')
+const Avatars = require('../models/Avatar');
 // const ClothType = require('../models/ClothType')
 // const HairType = require('../models/HairType')
 // const SkinType = require('../models/SkinType')
 // const PodcastSchema = require('../models/Podcast')
-const UserAuth = require('../models/UsersAuth')
-
+const User = require('../models/User');
 
 const addAvatar = async (req, res) => {
   //get user_id from request body
-  const {user_id}= req.body
+  const { user_id } = req.body;
   try {
     //find podcast that has same user_id
-    const podcast = await PodcastSchema.findOne({user_id})
+    const podcast = await PodcastSchema.findOne({ user_id });
     //append the id of podcast on the req.body
-    if(!podcast){
+    if (!podcast) {
       return res.status(404).json({
-        message:`no podcast with user id ${user_id}`
-      })
+        message: `no podcast with user id ${user_id}`,
+      });
     }
-    req.body.podcast_id=podcast._id
+    req.body.podcast_id = podcast._id;
     const newAvatar = await Avatars.create(req.body);
     res.status(201).json(newAvatar);
   } catch (error) {
     console.log(error);
-    res.status(500).json({error:error});
+    res.status(500).json({ error: error });
   }
 };
-const addCloth = async(req,res)=>{
+const addCloth = async (req, res) => {
   try {
     const newCloth = await ClothType.create(req.body);
     res.status(201).json(newCloth);
   } catch (error) {
     console.log(error);
-    res.status(500).json({error:error});
-  }  
-}
-const addHair = async(req,res)=>{
+    res.status(500).json({ error: error });
+  }
+};
+const addHair = async (req, res) => {
   try {
     const newHair = await HairType.create(req.body);
     res.status(201).json(newHair);
   } catch (error) {
     console.log(error);
-    res.status(500).json({error:error});
-  }  
-}
-const addSkin = async(req,res)=>{
+    res.status(500).json({ error: error });
+  }
+};
+const addSkin = async (req, res) => {
   try {
     const newSkin = await SkinType.create(req.body);
     res.status(201).json(newSkin);
   } catch (error) {
     console.log(error);
-    res.status(500).json({error:error});
-  }  
-}
+    res.status(500).json({ error: error });
+  }
+};
 
-
-
-module.exports={addAvatar, addCloth, addHair, addSkin}
+module.exports = { addAvatar, addCloth, addHair, addSkin };
