fe1afff80a79c0d462656409708f56f8ee4a205f
Samson Nwizugbe <samson6398@gmail.com>
2022-12-17 15:01:30 +0000

    Update conftest.py

--- pyhton-backend/tests/conftest.py
+++ pyhton-backend/tests/conftest.py
@@ -1,1 +1,27 @@
+import pytest
 
+# from unittest import mock
+from pathlib import Path
+
+
+ANIMATION_FRAME_LENGTH_LIST = [3, 5, 10, 15, 2, 7]
+
+AVATAR_MAP_LIST = [
+    {"A": Path("random/test/path")},
+    {"A": Path("random/test/path"), "B": Path("random/test/path/two")},
+    {
+        "A": Path("random/test/path"),
+        "B": Path("random/test/path/two"),
+        "C": Path("random/test/path/three"),
+    },
+]
+
+
+@pytest.fixture(params=ANIMATION_FRAME_LENGTH_LIST)
+def animation_frame_lenght(request):
+    yield request.param
+
+
+@pytest.fixture(params=AVATAR_MAP_LIST)
+def avatar_map_sample(request):
+    yield request.param
