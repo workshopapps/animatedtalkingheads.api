2d7a48a32edf0a2d71e670a342721dd97ed578ac
unknown <adetunmbikenny@gmail.com>
2022-12-21 09:49:39 +0100

    fix

--- node-backend/dist/models/UsersAuth.js
+++ node-backend/dist/models/UsersAuth.js
@@ -1,65 +1,53 @@
-"use strict";
+'use strict';
 const mongoose = require('mongoose');
-const { isEmail  } = require('validator');
+const { isEmail } = require('validator');
 const bcrypt = require('bcrypt');
 //const crypto = require("crypto");
 //const { stringify } = require('querystring');
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: [
-            true,
-            'Please enter an email'
-        ],
-        unique: true,
-        lowercase: true,
-        validate: [
-            isEmail,
-            'Please enter a valid email'
-        ]
-    },
-    password: {
-        type: String,
-        required: [
-            true,
-            'Please enter a password'
-        ],
-        minlength: [
-            6,
-            'Minimum password length is 6 characters'
-        ]
-    }
+  email: {
+    type: String,
+    required: [true, 'Please enter an email'],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, 'Please enter a valid email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: [6, 'Minimum password length is 6 characters'],
+  },
 });
 // fire a function before doc saved to db
-userSchema.pre('save', async function(next1) {
-    const salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password, salt);
-    next1();
+userSchema.pre('save', async function (next1) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next1();
 });
 // static method to login user
-userSchema.statics.login = async function(email, password) {
-    const user = await this.findOne({
-        email
-    });
-    if (user) {
-        const auth = await bcrypt.compare(password, user.password);
-        if (auth) {
-            return user;
-        }
-        throw Error('incorrect password');
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({
+    email,
+  });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
     }
-    throw Error('incorrect email');
+    throw Error('incorrect password');
+  }
+  throw Error('incorrect email');
 };
-userSchema.method.forgetpassword = async function(email, password) {
-    const user = await this.findOne({
-        email
-    });
-    if (user) {
-        const salt = await bcrypt.genSalt();
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    }
-    throw Error('incorrect email');
+userSchema.method.forgetpassword = async function (email, password) {
+  const user = await this.findOne({
+    email,
+  });
+  if (user) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  }
+  throw Error('incorrect email');
 };
-const UserAuth = mongoose.model('user', userSchema);
-module.exports = UserAuth;
+const User = mongoose.model('user', userSchema);
+module.exports = User;
